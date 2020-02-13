ebccb9d7406831714a8f0d3861f1a7de22df4123
gwanho <gwanhoyoon@gmail.com>
2020-02-13 19:27:06 +0900

    test

--- manage.py
+++ manage.py
@@ -2,10 +2,20 @@
 import os
 import sys
 import threading
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MyMain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        print(__name__)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     print(threading.current_thread())
+    app = QApplication(sys.argv)
+    a = MyMain()
+    a.show()
+    app.exec_()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
