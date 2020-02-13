caafc955763da90a46f4869371f5a3dcfe94c8ff
gwanho <gwanhoyoon@gmail.com>
2020-02-13 20:25:19 +0900

    test

--- test2.py
+++ test2.py
@@ -1,12 +1,16 @@
 from PyQt5.QtWidgets import QApplication
 import sys
 import threading
+import numpy
 
 if __name__ == "__main__":
     print(threading.current_thread())
     print(threading.main_thread())
     print('============================================================')
-    app = QApplication(sys.argv)
+    a = numpy.array([[1,2],[3,4]])
+    b = numpy.array([[1,2],[3,4]])
+    c = a+b
+    #app = QApplication(sys.argv)
     #a = MyMain()
     #a.show()
     #app.exec_()
