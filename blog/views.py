94b2db035b3342cb2a70d16c3c7dedea8c8eefd9
gwanho <gwanhoyoon@gmail.com>
2020-02-13 14:50:50 +0900

    test

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from .rcwa2 import *
+#from .rcwa2 import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import sys
+import runpy
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -18,21 +19,22 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
+    #if request.method == "POST":
+    #    form = PostForm(request.POST)
+    #    if form.is_valid():
+    #        post = form.save(commit=False)
+    #        post.author = request.user
+    #        post.published_date = timezone.now()
+    #        post.save()
+    #        return redirect('post_detail', pk=post.pk)
+    #else:
+    #    form = PostForm()
     #app = QApplication(sys.argv)
+    runpy.run_module('rcwa2', run_name='__main__')
     #a = MyMain()
     #a.show()
     #sys.exit(app.exec_())
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return #render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
--- rcwa2.py
+++ rcwa2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -9,6 +10,8 @@ class MyMain(QDialog):
     def __init__(self):
         super().__init__()
         self.setupUI()
+        print(__name__)
+        print('Hi!!!!')
         
     def setupUI(self):
         self.setGeometry(700, 400, 200, 100)
@@ -19,4 +22,15 @@ class MyMain(QDialog):
         layout = QVBoxLayout()
         layout.addWidget(img)
         self.setLayout(layout)
-        
+
+def main():
+    app = QApplication(sys.argv)
+    a = MyMain()
+    a.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+    #a = MyMain()
+    #a.show()
+    #sys.exit(app.exec_())
