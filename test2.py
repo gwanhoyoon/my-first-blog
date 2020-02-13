aadf2d93ffa9a8efd6b7e9821dded62eccd2dcfc
gwanho <gwanhoyoon@gmail.com>
2020-02-13 20:26:16 +0900

    test

--- test2.py
+++ test2.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     a = numpy.array([[1,2],[3,4]])
     b = numpy.array([[1,2],[3,4]])
     c = a+b
+    print(c)
     #app = QApplication(sys.argv)
     #a = MyMain()
     #a.show()
