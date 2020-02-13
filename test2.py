e6fc5ada6fab429fbbcb13c85890df2416ab43d7
gwanho <gwanhoyoon@gmail.com>
2020-02-13 17:08:27 +0900

    test

--- test2.py
+++ test2.py
@@ -1,9 +1,11 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 import sys
 import os
 import threading
 
 def main():
-    app = QApplication(sys.argv)
     print(threading.current_thread())
     print(threading.main_thread())
 
