8305da72364025bdf18c6b54a306b6f906cde6f8
gwanho <gwanhoyoon@gmail.com>
2020-02-13 18:58:44 +0900

    test

--- test2.py
+++ test2.py
@@ -2,22 +2,14 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 #from PyQt5.QtGui import *
 import sys
-import threading
 
 class MyMain(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         print(__name__)
 
-app = QApplication(sys.argv)
-a = MyMain()
-a.show()
-sys.exit(app.exec_())
-
-#if __name__ == "__main__":
-#    print(threading.current_thread())
-#    print(threading.main_thread())
-#    app = QApplication(sys.argv)
-#    a = MyMain()
-#    a.show()
-#    sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    a = MyMain()
+    a.show()
+    sys.exit(app.exec_())
