cd1c3a09ff9f28c5160f1c19d8325f379a914ef8
gwanho <gwanhoyoon@gmail.com>
2020-02-13 18:47:45 +0900

    test

--- test2.py
+++ test2.py
@@ -13,7 +13,7 @@ class MyMain(QMainWindow):
 if __name__ == "__main__":
     print(threading.current_thread())
     print(threading.main_thread())
-    app = QApplication(sys.argv)
+    #app = QApplication(sys.argv)
     #a = MyMain()
     #a.show()
     #sys.exit(app.exec_())
