9c45a143053de2bab4670340a1ce93dc22b73753
gwanho <gwanhoyoon@gmail.com>
2020-02-13 19:29:48 +0900

    test

--- test2.py
+++ test2.py
@@ -1,11 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-class MyMain(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        print(__name__)
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     #a = MyMain()
