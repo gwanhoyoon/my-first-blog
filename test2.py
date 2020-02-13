a1f94d375bef13504126c004820e6d8d69241980
gwanho <gwanhoyoon@gmail.com>
2020-02-13 18:42:25 +0900

    test

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+#from PyQt5.QtCore import *
+#from PyQt5.QtWidgets import *
+#from PyQt5.QtGui import *
+import PyQt5
 import numpy
 import matplotlib
 import sys
 import os
 import threading
 
-class MyMain(QMainWindow):
+class MyMain(PyQt5.QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         print(__name__)
@@ -15,7 +16,7 @@ class MyMain(QMainWindow):
 if __name__ == "__main__":
     print(threading.current_thread())
     print(threading.main_thread())
-    app = QApplication(sys.argv)
+    app = PyQt5.QtGui.QApplication(sys.argv)
     #a = MyMain()
     #a.show()
     #sys.exit(app.exec_())
